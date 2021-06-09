d9095a665cfb80ccf5a2e14c049fc9a257d4fffc
Gustavo Esteban <gustavoesteban13@hotmail.com>
2021-06-09 18:43:24 -0300

    feat: Inicio do projeto

---
+++ netflix/src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  //Deu erro vou ver isso dps
+
+  // listTitles = [{
+  //   trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+  //   cover: 'assets/images/guardios-da-galaxia.jpg',
+  //   name: 'Guardiões da Galaxia',
+  //   relevance: 98,
+  //   age: 0,
+  //   parts: 2,
+  //   categories: ['Épico','Filme de fantasia','Viagem no espaço']
+
+  // },{
+  //   trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+  //   cover: 'assets/images/guardios-da-galaxia.jpg',
+  //   name: 'Guardiões da Galaxia 2',
+  //   relevance: 80,
+  //   age: 12,
+  //   parts: 2,
+  //   categories: ['Épico','Filme de fantasia','Viagem no espaço']
+
+  // }, {
+  //   trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+  //   cover: 'assets/images/guardios-da-galaxia.jpg',
+  //   name: 'Guardiões da Galaxia 3',
+  //   relevance: 55,
+  //   age: 18,
+  //   parts: 2,
+  //   categories: ['Épico','Filme de fantasia','Viagem no espaço']
+
+  // }, {
+  //   trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+  //   cover: 'assets/images/guardios-da-galaxia.jpg',
+  //   name: 'Guardiões da Galaxia 3',
+  //   relevance: 55,
+  //   age: 18,
+  //   parts: 2,
+  //   categories: ['Épico','Filme de fantasia','Viagem no espaço']
+
+  // }];
+  
+}
